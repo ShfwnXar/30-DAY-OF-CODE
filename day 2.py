fa58aae67c5f62c80b5bf8c352703c4da43dc111
ShfwnXar <safwanauliarahman@gmail.com>
2024-06-02 06:46:53 +0700

    Create day 2.py
    day 2 02/06/2024

---
+++ day 2.py
@@ -0,0 +1,24 @@
+# Pengenalan Variabel dalam Python
+
+# Mendefinisikan variabel
+nama = "Shafwan"  # String
+umur = 20       # Integer
+tinggi = 170.0 # Float
+dia_mahasiswa = True # Boolean
+
+# Menampilkan nilai variabel
+print("Nama:", nama)
+print("Usia:", umur)
+print("Tinggi:", tinggi)
+print("Apakah seorang mahasiswa?", dia_mahasiswa)
+
+# Mengubah nilai variabel
+nama = "aulia"
+umur = 20
+dia_mahasiswa = False
+
+# Menampilkan nilai variabel yang telah diubah
+print("\nSetelah perubahan:")
+print("Nama:", nama)
+print("Usia:", umur)
+print("Apakah seorang mahasiswa?", dia_mahasiswa)
