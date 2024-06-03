e1e38930e99b359237b32b91cfa165f934fa1da0
ShfwnXar <safwanauliarahman@gmail.com>
2024-06-03 10:18:22 +0700

    Create day 3.py
    day 3 03/06/2024

---
+++ day 3.py
@@ -0,0 +1,31 @@
+# Pengenalan Operasi Matematika dalam Python
+
+# Penjumlahan
+a = 10
+b = 5
+hasiljumlah = a + b
+print("Penjumlahan:", hasiljumlah)
+
+# Pengurangan
+hasilq = a - b
+print("Pengurangan:",hasilq)
+
+# Perkalian
+hasilx = a * b
+print("Perkalian:", hasilx)
+
+# Pembagian
+hasilb = a / b
+print("Pembagian:", hasilb)
+
+# Pembagian Bulat
+hasilbb = a // b
+print("Pembagian Bulat:", hasilbb)
+
+# Modulus (Sisa Pembagian)
+hasilm = a % b
+print("Modulus:", hasilm)
+
+# Pangkat
+hasilp = a ** b
+print("Pangkat:", hasilp)
